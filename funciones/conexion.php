2777779e762515c8b895ebb48dfd65cee19530da
Juangarck <juangarciagrracc@gmail.com>
2024-09-23 01:21:58 -0500

    Se modifican las credenciales de correo y se agregan que las conexiones sean UTF8

--- funciones/conexion.php
+++ funciones/conexion.php
@@ -4,4 +4,6 @@
 	$password='';
 	$db='turnero';
 	$con=mysqli_connect($host,$user,$password,$db) or die ("<span class='mensaje'>Error al conectar con la base de datos</span>");
+    // Establecer la codificación UTF-8 para la conexión
+    mysqli_set_charset($con, "utf8");
 ?>
